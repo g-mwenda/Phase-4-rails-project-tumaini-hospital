a95abfd5e7178489cac64ca8e339cefff894c672
henry cheche <henry.cheche@student.moringaschool.com>
2023-07-02 23:03:11 +0300

    Added migration files

--- db/migrate/20230702195656_create_patients.rb
+++ db/migrate/20230702195656_create_patients.rb
@@ -1,6 +1,13 @@
 class CreatePatients < ActiveRecord::Migration[7.0]
   def change
-    create_table :patients do |t|
+    create_table :posts do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone
+      t.string :condition
+      t.text :notes
+      t.boolean :archive, default: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
