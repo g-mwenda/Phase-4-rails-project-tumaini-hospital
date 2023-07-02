a7911e4d937d2646f23737819c0bc4d148aa9541
henry cheche <henry.cheche@student.moringaschool.com>
2023-07-02 23:05:54 +0300

    Added relationships

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,8 @@
 class Appointment < ApplicationRecord
+    belongs_to :user
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :date, presence: true
+    validates :time, presence: true
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+    belongs_to :user
+    validates :name, presence: true
+    validates :phone, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :notes, presence: true
+    validates :condition, presence: true
 end
